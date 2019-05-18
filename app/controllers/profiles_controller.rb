4804049b9d144a9d8126a7ad14c262e4e7e658c1
Stephen Boyd <adewaleas@alldaylolz.com>
2019-05-18 07:33:35 +0000

    Added update action to llow user to edit their profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,11 +11,30 @@ class ProfilesController < ApplicationController
         @profile = @user.build_profile( profile_params )
         if @profile.save
             flash[:success] ="Profile Updated!"
-            redirect_to user_path( params[:user_id] )
+            redirect_to user_path(id: params[:user_id] )
         else
             render action :new
         end
     end
+    
+    def edit 
+        @user =User.find( params[:user_id] )
+        @profile = @user.profile
+    end
+    
+    #PATCH to /users/:user_id/profile
+    def update
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+        if @profile.update_attributes(profile_params)
+            flash[:success] = "Profile Updated!"
+            redirect_to user_path(id: params[:user_id] )
+        else
+            render action: :edit
+        end
+            
+    end
+    
      private
      def profile_params
         params.require(:profile).permit(:first_name, :last_name, :avatar,  :job_title, :phone_number, :contact_email, :description) 
